1f846755802988147bb2950ed52c64510fbc6b3d
Rajat dua <48120175+rajatdua77@users.noreply.github.com>
2020-02-06 12:14:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+
+    $(document).ready(()=>{
+        $('#btn1').click(()=>{
+          var age=document.getElementById('Age').value;
+
+          var retireAge=document.getElementById('retireAge').value;
+        
+          if(age<=100){
+        
+          }
+          else if(age<=100){
+        
+          }
+          var retirement=parseFloat(retireAge-age);
+          var result=new Date().getFullYear() + retirement;
+        
+          document.getElementById('finalScoreText').innerHTML="You are "+ age+ " Years old.";
+          document.getElementById('retire').innerHTML="You want to be "+retireAge+" years old  when you will retire.";
+          document.getElementById('result').innerHTML="In "+result+" You can retire.";
+          
+          $("#results").css({"display":"block"});
+          $(".final").css({"display":"block"});
+           
+        })
+    })
